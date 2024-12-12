bcc7bc0059e42fa3e162e467293bc72c17f5f824
Hyps <me@hypsster.com>
2024-12-12 02:10:59 -0500

    add configuration file and read keys from it in main function

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
 int main() {
+    int key1 = 0;
+    int key2 = 0;
+    FILE *configFile = fopen("config.txt", "r");
+    if (configFile == NULL) {
+        perror("Error opening config file");
+        return 1;
+    }
+
+    if (fscanf(configFile, "%d %d", &key1, &key2) != 2) {
+        perror("Error reading config file");
+        fclose(configFile);
+        return 1;
+    }
+    fclose(configFile);
+    printf("keyA: %d, keyB: %d\n", key1, key2);
+
     return 0;
 }
